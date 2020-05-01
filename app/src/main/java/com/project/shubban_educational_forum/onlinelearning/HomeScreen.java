0c6c323195ffda54348302c6fe46a5334e28a83a
jhanzaibdeveloper <64661299+jhanzaibdeveloper@users.noreply.github.com>
2020-05-02 01:55:54 +0500

    removed

--- app/src/main/java/com/project/shubban_educational_forum/onlinelearning/HomeScreen.java
+++ app/src/main/java/com/project/shubban_educational_forum/onlinelearning/HomeScreen.java
@@ -11,7 +11,7 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        jhanzaib typed by usman
+        
 
     }
 }
