5023dba6edb45caae4b030f107ce5d72f2adf20b
Usman Sh <usman.shahzadd@gmail.com>
2020-06-01 23:37:13 +0500

    Lecture 1 (6/1/20)

--- app/src/main/java/com/project/shubban_educational_forum/onlinelearning/HomeScreen.java
+++ app/src/main/java/com/project/shubban_educational_forum/onlinelearning/HomeScreen.java
@@ -3,13 +3,55 @@ package com.project.shubban_educational_forum.onlinelearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HomeScreen extends AppCompatActivity {
+
+    ListView listView;
+
+    ArrayAdapter<String> adapter;
+
+    ArrayList<String> studentNames = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+
+
+        listView = findViewById(R.id.listView);
+
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, studentNames);
+
+
+        listView.setAdapter(adapter);
+
+        studentNames.add("Usman sh");
+        studentNames.add("Jhanzaib");
+        studentNames.add("Ali");
+        studentNames.add("Bilal");
+        studentNames.add("M.Javaid");
+        studentNames.add("Usman sh");
+        studentNames.add("Jhanzaib");
+        studentNames.add("Ali");
+        studentNames.add("Bilal");
+        studentNames.add("M.Javaid");
+        studentNames.add("Usman sh");
+        studentNames.add("Jhanzaib");
+        studentNames.add("Ali");
+        studentNames.add("Bilal");
+        studentNames.add("M.Javaid");
+
+        //List
+        //1. ListView
+        //2. ArrayAdapter
+        //3. ArrayList (Data)
 
 
     }
--- app/src/main/java/com/project/shubban_educational_forum/onlinelearning/MainActivity.java
+++ app/src/main/java/com/project/shubban_educational_forum/onlinelearning/MainActivity.java
@@ -2,14 +2,41 @@ package com.project.shubban_educational_forum.onlinelearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button gonextBt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Step1: Create UI
+        //Step2: Link component
+        //Step3: Add Action
+        //Step4: Add code as per your requirement
+
+        gonextBt = findViewById(R.id.gonextBt);
+
+
+        gonextBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //When user will press this button then following code will run
+                Intent intent = new Intent(MainActivity.this,HomeScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
 
 
     }
