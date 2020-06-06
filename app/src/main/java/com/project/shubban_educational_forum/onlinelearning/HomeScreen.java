ea319dcee9776873a819026cd2c26d1b09662734
Usman Sh <usman.shahzadd@gmail.com>
2020-06-06 11:13:08 +0500

    Url loading in ArrayAdapter

--- app/src/main/java/com/project/shubban_educational_forum/onlinelearning/HomeScreen.java
+++ app/src/main/java/com/project/shubban_educational_forum/onlinelearning/HomeScreen.java
@@ -2,9 +2,14 @@ package com.project.shubban_educational_forum.onlinelearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,43 +21,64 @@ public class HomeScreen extends AppCompatActivity {
 
     ArrayList<String> studentNames = new ArrayList<>();
 
+    ArrayList<String> urlsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-
-
-        listView = findViewById(R.id.listView);
-
-
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, studentNames);
-
-
-        listView.setAdapter(adapter);
-
-        studentNames.add("Usman sh");
-        studentNames.add("Jhanzaib");
-        studentNames.add("Ali");
-        studentNames.add("Bilal");
-        studentNames.add("M.Javaid");
-        studentNames.add("Usman sh");
-        studentNames.add("Jhanzaib");
-        studentNames.add("Ali");
-        studentNames.add("Bilal");
-        studentNames.add("M.Javaid");
-        studentNames.add("Usman sh");
-        studentNames.add("Jhanzaib");
-        studentNames.add("Ali");
-        studentNames.add("Bilal");
-        studentNames.add("M.Javaid");
-
         //List
         //1. ListView
         //2. ArrayAdapter
         //3. ArrayList (Data)
+        //4. List Item Click Listner
 
 
+
+
+        //Adding URLs into ArrayList
+        urlsList.add("https://www.youtube.com/watch?v=GX9gzEOVNMA");
+        urlsList.add("https://www.youtube.com/watch?v=z1PSbDmV8Gw");
+        urlsList.add("https://www.youtube.com/watch?v=T9_GW5ehViA");
+        urlsList.add("https://www.youtube.com/watch?v=26U_seo0a1g");
+        urlsList.add("https://help.twitter.com/en/using-twitter/tweeting-gifs-and-pictures");
+
+
+
+
+        listView = findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, urlsList);
+
+        listView.setAdapter(adapter);
+
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                String loadUrl = urlsList.get(position);
+
+                Toast.makeText(HomeScreen.this, "URL Position: "+loadUrl, Toast.LENGTH_SHORT).show();
+
+                Intent openURL = new Intent(Intent.ACTION_VIEW, Uri.parse(loadUrl));
+                startActivity(openURL);
+
+
+            }
+        });
+
+
+
+
+
+
+//        studentNames.add("Usman sh");
+//        studentNames.add("Jhanzaib");
+
+
+//
     }
 }
